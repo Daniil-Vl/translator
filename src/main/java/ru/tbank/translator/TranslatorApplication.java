d13bb9721a6305ccaf6b82733f48ab20dc4c91ad
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-08-03 18:49:50 +0300

    update TranslatorApplication

--- src/main/java/ru/tbank/translator/TranslatorApplication.java
+++ src/main/java/ru/tbank/translator/TranslatorApplication.java
@@ -2,12 +2,13 @@ package ru.tbank.translator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.tbank.translator.configuration.ApplicationConfig;
 
+@EnableConfigurationProperties(ApplicationConfig.class)
 @SpringBootApplication
 public class TranslatorApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TranslatorApplication.class, args);
     }
-
 }
