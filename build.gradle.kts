28ab77e7c376db9c022a7ab2b12f4cd0e633345e
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-08-03 13:05:11 +0300

    add assertj lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.assertj:assertj-core:3.11.1")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
