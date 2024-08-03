222ad6a83f5d4ce5a4717921d0991f99a851d311
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-08-03 14:25:55 +0300

    add dtos

---
+++ src/main/java/ru/tbank/translator/dto/LanguageDto.java
@@ -0,0 +1,4 @@
+package ru.tbank.translator.dto;
+
+public record LanguageDto(String name, String fullname) {
+}
---
+++ src/main/java/ru/tbank/translator/dto/UserDto.java
@@ -0,0 +1,4 @@
+package ru.tbank.translator.dto;
+
+public record UserDto(String ip) {
+}
