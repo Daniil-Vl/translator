6bebb7b222553ca8c8542ac751e40071cdfb12ed
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-08-04 14:02:37 +0300

    add mocked yandex api data to application config so that test context can run without problems

--- src/test/java/ru/tbank/translator/TranslatorApplicationTests.java
+++ src/test/java/ru/tbank/translator/TranslatorApplicationTests.java
@@ -2,8 +2,16 @@ package ru.tbank.translator;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 public class TranslatorApplicationTests {
+    @DynamicPropertySource
+    static void yandexMockConfig(DynamicPropertyRegistry registry) {
+        registry.add("app.yandex-translate-url", () -> "");
+        registry.add("app.yandex-detect-language-url", () -> "");
+        registry.add("app.yandex-api-key", () -> "");
+    }
 }
