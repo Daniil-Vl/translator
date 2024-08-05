399c8362e3b20f9af9eb58fafdd4f6e60ee7d84e
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-08-05 21:27:38 +0300

    add LanguageNotDetectedException and not detected language type in YandexTranslateApiClientImpl

--- src/main/java/ru/tbank/translator/client/impl/YandexTranslateApiClientImpl.java
+++ src/main/java/ru/tbank/translator/client/impl/YandexTranslateApiClientImpl.java
@@ -2,10 +2,13 @@ package ru.tbank.translator.client.impl;
 
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import ru.tbank.translator.client.TranslateApiClient;
 import ru.tbank.translator.configuration.ApplicationConfig;
 import ru.tbank.translator.dto.yandex_translate.*;
+import ru.tbank.translator.exceptions.LanguageNotDetectedException;
+import ru.tbank.translator.exceptions.TranslateApiUnavailableException;
 
 import java.net.URI;
 import java.util.List;
@@ -65,15 +68,6 @@ public class YandexTranslateApiClientImpl implements TranslateApiClient {
         );
     }
 
-    private YandexTranslateResponseDto getTranslatedWord(TranslateRequest bodyDto) {
-        HttpEntity<TranslateRequest> httpEntity = new HttpEntity<>(bodyDto, httpHeaders);
-
-        ResponseEntity<YandexTranslateResponseDto> result = restTemplate.exchange(
-                translateApiUrl, HttpMethod.POST, httpEntity, YandexTranslateResponseDto.class);
-
-        return result.getBody();
-    }
-
     @Override
     public String detectLanguage(String word) {
         DetectLanguageRequest bodyDto = new DetectLanguageRequest(word);
@@ -81,8 +75,53 @@ public class YandexTranslateApiClientImpl implements TranslateApiClient {
         HttpEntity<DetectLanguageRequest> httpEntity = new HttpEntity<>(bodyDto, httpHeaders);
 
         ResponseEntity<DetectLanguageResponse> result = restTemplate.exchange(
-                detectLanguageApiUrl, HttpMethod.POST, httpEntity, DetectLanguageResponse.class);
+                detectLanguageApiUrl, HttpMethod.POST, httpEntity, DetectLanguageResponse.class
+        );
 
         return result.getBody().languageCode();
+    }
+
+    private YandexTranslateResponseDto getTranslatedWord(TranslateRequest bodyDto) {
+        HttpEntity<TranslateRequest> httpEntity = new HttpEntity<>(bodyDto, httpHeaders);
+
+        // It is necessary to handle errors here, because it is impossible to receive an error message in ResponseErrorHandler
+        try {
+            return restTemplate
+                    .exchange(translateApiUrl, HttpMethod.POST, httpEntity, YandexTranslateResponseDto.class)
+                    .getBody();
+        } catch (HttpClientErrorException exc) {
+            if (exc.getStatusCode().is5xxServerError())
+                throw new TranslateApiUnavailableException();
+
+            if (exc.getStatusCode().equals(HttpStatus.BAD_REQUEST))
+                throw getCause(exc.getMessage());
+
+            throw new RuntimeException(exc);
+        }
+    }
+
+    /**
+     * Determines which of the languages was not found and throws an exception with the corresponding message.
+     *
+     * @param errorMessage - error message from yandex translate api
+     * @return exception with corresponding message
+     */
+    private LanguageNotDetectedException getCause(String errorMessage) {
+        /*
+        Message from yandex api looks like this:
+              400 Bad Request: "{<EOL> "code": 3,<EOL> "message": "unsupported target_language_code: fu",<EOL> ...
+         or this:
+              400 Bad Request: "{<EOL> "code": 3,<EOL> "message": "unsupported source_language_code: fu",<EOL> ...
+        */
+
+        String languageType = errorMessage.substring(65, 71);
+
+        String exceptionMessage = switch (languageType) {
+            case "target" -> LanguageNotDetectedException.TARGET_LANGUAGE_NOT_DETECTED_MESSAGE;
+            case "source" -> LanguageNotDetectedException.SOURCE_LANGUAGE_NOT_DETECTED_MESSAGE;
+            default -> throw new IllegalStateException("Unexpected value: " + languageType);
+        };
+
+        return new LanguageNotDetectedException(exceptionMessage);
     }
 }
