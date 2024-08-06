30c9cf9351d907b219cda779250218551f7d7471
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-08-06 19:42:00 +0300

    implemented the addition of a new user if he uses the translator for the first time

--- src/main/java/ru/tbank/translator/controller/impl/TranslatorControllerImpl.java
+++ src/main/java/ru/tbank/translator/controller/impl/TranslatorControllerImpl.java
@@ -2,11 +2,12 @@ package ru.tbank.translator.controller.impl;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 import ru.tbank.translator.controller.TranslatorController;
 import ru.tbank.translator.dao.model.Translation;
 import ru.tbank.translator.dao.repository.TranslationRepository;
-import ru.tbank.translator.dto.TranslationDto;
+import ru.tbank.translator.dao.repository.UserRepository;
 import ru.tbank.translator.dto.controller_dto.TranslateRequest;
 import ru.tbank.translator.dto.controller_dto.TranslateResponse;
 import ru.tbank.translator.service.TranslationService;
@@ -16,13 +17,17 @@ import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 public class TranslatorControllerImpl implements TranslatorController {
 
     private final TranslationService translationService;
     private final TranslationRepository translationRepository;
+    private final UserRepository userRepository;
 
     @Override
     public TranslateResponse translate(TranslateRequest requestBody, HttpServletRequest httpServletRequest) throws ExecutionException, InterruptedException {
+        String userIp = httpServletRequest.getRemoteAddr();
+
         var translateResponse = translationService.translateSentence(
                 requestBody.text(),
                 requestBody.sourceLanguage(),
@@ -34,9 +39,15 @@ public class TranslatorControllerImpl implements TranslatorController {
                 translateResponse.translatedText(),
                 translateResponse.sourceLanguage(),
                 requestBody.targetLanguage(),
-                httpServletRequest.getRemoteAddr(),
+                userIp,
                 OffsetDateTime.now()
         );
+
+        if (!userRepository.isExists(userIp)) {
+            log.info("New user with ip - {}", userIp);
+            userRepository.addUser(userIp);
+        }
+
         translationRepository.addTranslation(translation);
 
         return new TranslateResponse(
